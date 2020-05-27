c65155886f771b742355e9018c37c4558cafa617
Lakeisha McCree <lmccree215@icloud.com>
2020-05-27 03:23:02 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
 def parrot(string="Squawk!")
+  puts #{string}
   
-  puts "Squawk!"
-  phrase = "Squawk!"
-  return phrase
-  end
 end
